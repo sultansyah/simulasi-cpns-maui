550972d6b79a75fb709c39941ac55a53bbb62227
sultansyah <muhammadsultansyah14@gmail.com>
2026-06-18 23:00:01 +0700

    update mascot

--- SimulasiCPNS/Views/HomePage.xaml.cs
+++ SimulasiCPNS/Views/HomePage.xaml.cs
@@ -35,6 +35,8 @@ public partial class HomePage : ContentPage
         _isMascotAnimationRunning = false;
         MascotContainer.AbortAnimation("MascotBob");
         MascotContainer.AbortAnimation("MascotBlink");
+        MascotLampGlowOuter.AbortAnimation("MascotLampOuter");
+        MascotLampGlowInner.AbortAnimation("MascotLampInner");
     }
 
     private void StartMascotAnimation()
@@ -52,6 +54,8 @@ public partial class HomePage : ContentPage
     {
         while (_isMascotAnimationRunning)
         {
+            _ = AnimateLampPulseAsync();
+
             await MascotContainer.TranslateTo(0, -5, 850, Easing.SinInOut);
             await MascotContainer.ScaleTo(1.03, 850, Easing.SinInOut);
             await MascotContainer.RotateTo(-3, 850, Easing.SinInOut);
@@ -63,5 +67,20 @@ public partial class HomePage : ContentPage
             await MascotContainer.RotateTo(0, 500, Easing.SinOut);
             await Task.Delay(150);
         }
+    }
+
+    private async Task AnimateLampPulseAsync()
+    {
+        await Task.WhenAll(
+            MascotLampGlowOuter.FadeTo(0.34, 280, Easing.SinOut),
+            MascotLampGlowOuter.ScaleTo(1.12, 280, Easing.SinOut),
+            MascotLampGlowInner.FadeTo(0.88, 280, Easing.SinOut),
+            MascotLampGlowInner.ScaleTo(1.08, 280, Easing.SinOut));
+
+        await Task.WhenAll(
+            MascotLampGlowOuter.FadeTo(0.12, 520, Easing.SinInOut),
+            MascotLampGlowOuter.ScaleTo(1.0, 520, Easing.SinInOut),
+            MascotLampGlowInner.FadeTo(0.42, 520, Easing.SinInOut),
+            MascotLampGlowInner.ScaleTo(1.0, 520, Easing.SinInOut));
     }
 }
